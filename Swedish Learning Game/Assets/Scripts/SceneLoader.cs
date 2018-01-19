c3a939e3dbedf8892a3bf70dab8c1a6951335812
Juha Aarnio <juha.aarnio@live.fi>
2018-01-19 11:08:22 +0200

    Test push
    Basic structure

---
+++ Swedish Learning Game/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public Button latvia;
+    public Button finland;
+    string sceneToLoad;
+    
+    // Use this for initialization
+    void Start () {
+        DontDestroyOnLoad(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SceneSelectionFinland()
+    {
+        sceneToLoad = "Finland";
+    }
+
+    public void SceneSelectionLatvia()
+    {
+        sceneToLoad = "Latvia";
+    }
+
+    public void ButtonAction()
+    {
+        StartCoroutine(LoadScene(sceneToLoad));
+    }
+
+    IEnumerator LoadScene(string scene)
+    {
+        AsyncOperation sceneLoad = SceneManager.LoadSceneAsync(scene);
+        if (!sceneLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+}
