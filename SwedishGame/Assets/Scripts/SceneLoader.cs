f21aa618e9c593ae3677c0dbd87224bab1b7fed9
Juha Aarnio <juha.aarnio@live.fi>
2018-03-05 15:54:36 +0200

    Starting work on city screen
    Crude version of the city screen. All scenes should now be linked to the map of Latvia.

---
+++ SwedishGame/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class SceneLoader : MonoBehaviour {
+
+        public void ClickButton()
+        {
+                string scene = EventSystem.current.currentSelectedGameObject.name;
+                StartCoroutine(LoadScene(scene));
+        }
+
+        IEnumerator LoadScene(string scene)
+        {
+                AsyncOperation loadScene = SceneManager.LoadSceneAsync(scene);
+                while (!loadScene.isDone)
+                    yield return null;
+        }
+}
