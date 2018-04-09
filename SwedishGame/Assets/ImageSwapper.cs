3cb3687da5cf0599d1d8be784021e4690514b2a8
Juha Aarnio <juha.aarnio@live.fi>
2018-04-09 20:58:29 +0300

    Added more functionality to Riga.

---
+++ SwedishGame/Assets/ImageSwapper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageSwapper : MonoBehaviour {
+
+    public Sprite[] sprites;
+    public Image imageContainer;
+    PlayerStats stats = PlayerStats.instance;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (stats.selectedGender == "male")
+        {
+            imageContainer.sprite = sprites[0];
+        }
+        else
+        {
+            imageContainer.sprite = sprites[1];
+        }
+	}
+	
+	
+}
