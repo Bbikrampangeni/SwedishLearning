8c8efe4a7aa8b120e7147cac6d9daef4523afa75
Taikapipari <juha.aarnio@live.fi>
2018-04-10 08:41:26 +0300

    Phams stuff

--- SwedishGame/Assets/Book-Page Curl/scripts/Book.cs
+++ SwedishGame/Assets/Book-Page Curl/scripts/Book.cs
@@ -101,9 +101,12 @@ public class Book : MonoBehaviour {
         {
             UpdateBook();
         }
+
+        Debug.Log(currentPage);
         //Debug.Log("mouse local pos:" + transformPoint(Input.mousePosition));
         //Debug.Log("mouse  pos:" + Input.mousePosition);
     }
+    
     public void UpdateBook()
     {
         f= Vector3.Lerp(f,transformPoint( Input.mousePosition), Time.deltaTime * 10);
