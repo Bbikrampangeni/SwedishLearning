ba818ae61d81f5a450afbdcb061e94c549930797
Bbikrampangeni <33446541+Bbikrampangeni@users.noreply.github.com>
2018-04-10 15:43:58 +0300

    food changes
    food

--- main/Assets/email_system.cs
+++ main/Assets/email_system.cs
@@ -14,7 +14,8 @@ public class email_system : MonoBehaviour
     public GameObject email1;
     public GameObject subject1;
     public GameObject error;
-
+    public GameObject sent;
+    public GameObject clearsocial;
 
 
 
@@ -29,7 +30,10 @@ public class email_system : MonoBehaviour
         string body = subject1.GetComponent<Text>().text;
         if (email != "")
         {
-            Application.OpenURL("mailto:" + email + "?subject=" + subject + "&body=" + body + "\n" + end);
+            Application.OpenURL("mailto:" + email + "?subject=" + subject + "&body=" + body + "\n \n" + end);
+            sent.gameObject.SetActive(true);
+            error.gameObject.SetActive(false);
+            clearsocial.gameObject.SetActive(false);
 
         }
         else
